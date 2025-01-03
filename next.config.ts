0db0337ee6e7e26a9ae6b23b23f6bc091e967a6a
adivversano <adivversano@gmail.com>
2025-01-03 22:20:32 +0200

    build14

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/aviv-cv",
+  assetPrefix: "/aviv-cv",
   output: 'export',
   // distDir: 'dist',
   images: {
     unoptimized: true
-  }
+  },
+  env: {
+    BASE_URL: 'https://adivversano.github.io/aviv-cv/',
+  },
 };
 
 export default nextConfig;
